b9cc6ad9653756193de6ee87750f406b1c7c27a6
carto1111 <68772634+carto1111@users.noreply.github.com>
2021-10-05 00:25:49 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,26 @@
 #!/bin/sh
 
+# configs
+AUUID=ed7bc0c9-b9f9-49f8-a57a-5d962a9dbb89
+CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
+CONFIGCADDY=https://raw.githubusercontent.com/carto1111/HTC/main/etc/Caddyfile
+CONFIGXHCT=https://raw.githubusercontent.com/carto1111/HTC/master/etc/xhct.json
+ParameterSSENCYPT=chacha20-ietf-poly1305
+StoreFiles=https://raw.githubusercontent.com/carto1111/HTC/main/etc/StoreFiles
+#PORT=4433
+mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
+wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
+wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
+wget -qO- $CONFIGXHCT | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/xhct.json
 
-/hopol -config /cyld.json
+# storefiles
+mkdir -p /usr/share/caddy/$AUUID && wget -O /usr/share/caddy/$AUUID/StoreFiles $StoreFiles
+wget -P /usr/share/caddy/$AUUID -i /usr/share/caddy/$AUUID/StoreFiles
+
+for file in $(ls /usr/share/caddy/$AUUID); do
+    [[ "$file" != "StoreFiles" ]] && echo \<a href=\""$file"\" download\>$file\<\/a\>\<br\> >>/usr/share/caddy/$AUUID/ClickToDownloadStoreFiles.html
+done
+# start
+tor &
+/hopol -config /xhct.json &
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
