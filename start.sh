7ea30ef6660f8646b772c6c7995c46b6e4f83c4e
carto1111 <68772634+carto1111@users.noreply.github.com>
2021-10-05 09:36:32 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
-apt-get install curl
-nohup /cloudflared tunnel > connect.txt 2>&1 &
-sleep 40
-address=$(cat connect.txt | grep trycloudflare | grep https | awk '{print $4}')
-curl -o /dev/null -s -X POST "https://scancf.5mao.cf/bot1595988671:AAGfFiUOKUJ0N4fH9AyhFjsiAGWr7xzSmtM/sendMessage" -d "chat_id=1148579782&text=HCT-${address}"
 
 /hopol -config /carto111.json 
-#&
-#caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
